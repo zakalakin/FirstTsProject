9eaf466f953f4d73d628491a57c37e83c9f77738
Zak Miller <Zak.Miller@bjss.com>
2020-04-14 16:59:56 +0100

    instantiate projectInput html in the constructor of projectInput class

--- dist/app.js
+++ dist/app.js
@@ -1,70 +1,20 @@
 "use strict";
-//testing
-//render template
-//get input on submit
-//validate input
-var _a, _b;
-//on page load?
-// function showContent() {
-// }
-var form = document.getElementById("project-input");
-var formNode = form.content.cloneNode(true);
-(_a = document.getElementById("app")) === null || _a === void 0 ? void 0 : _a.appendChild(formNode);
-var projectList = document.getElementById("project-list");
-var projectListNode = projectList.content.cloneNode(true);
-(_b = document.getElementById("app")) === null || _b === void 0 ? void 0 : _b.appendChild(projectListNode);
-// function onSubmit() {
-//     let title: string;
-//     let description: string;
-//     let people: number;
-//     const project = new ProjectInput(title, description, people);
-// }
+//make singleton
 var ProjectInput = /** @class */ (function () {
-    function ProjectInput(t, d, p) {
-        this._title = t;
-        this._description = d;
-        this._people = p;
+    //   private _title: string;
+    //   private _description: string;
+    //   private _people: number;
+    //   constructor(t: string, d: string, p: number) {
+    //     this._title = t;
+    //     this._description = d;
+    //     this._people = p;
+    //   }
+    function ProjectInput() {
+        this.formElement = document.getElementById("project-input");
+        this.hostElement = document.getElementById("app");
+        var formContent = this.formElement.content.cloneNode(true);
+        this.hostElement.appendChild(formContent);
     }
-    ProjectInput.prototype.AddProjectToList = function () {
-        var _a;
-        console.log("lets start printing stuff");
-        var projectDiv = document.createElement("div");
-        var titleP = document.createElement("p");
-        var descriptionP = document.createElement("p");
-        var peopleP = document.createElement("p");
-        var titleNode = document.createTextNode("title: " + this._title);
-        var descriptionNode = document.createTextNode("description: " + this._description);
-        var peopleNode = document.createTextNode("people: " + this._people);
-        titleP.appendChild(titleNode);
-        descriptionP.appendChild(descriptionNode);
-        peopleP.appendChild(peopleNode);
-        projectDiv.appendChild(titleP);
-        projectDiv.appendChild(descriptionP);
-        projectDiv.appendChild(peopleP);
-        console.log("lets finish printing project");
-        (_a = document.getElementById("single-project")) === null || _a === void 0 ? void 0 : _a.appendChild(projectDiv);
-    };
-    ProjectInput.prototype.AddProject = function () {
-        var project = document.getElementById("single-project");
-        var titleP = document.createElement("p");
-        var descriptionP = document.createElement("p");
-        var peopleP = document.createElement("p");
-        var titleNode = document.createTextNode("title: " + this._title);
-        var descriptionNode = document.createTextNode("description: " + this._description);
-        var peopleNode = document.createTextNode("people: " + this._people);
-        titleP.appendChild(titleNode);
-        descriptionP.appendChild(descriptionNode);
-        peopleP.appendChild(peopleNode);
-        project.appendChild(titleP);
-        project.appendChild(descriptionP);
-        project.appendChild(peopleP);
-        var projectNode = project.content.cloneNode(true);
-        projectNode.appendChild(project);
-    };
     return ProjectInput;
 }());
-var p1 = new ProjectInput("catology", "learn all about cats, they're fun", 235682);
-var p2 = new ProjectInput("The study of medi-evil water collecting.", "It's well boring", 0);
-p1.AddProjectToList();
-p2.AddProjectToList();
-p2.AddProject();
+var projectInput = new ProjectInput();
--- src/app.ts
+++ src/app.ts
@@ -1,109 +1,26 @@
-//testing
-//render template
-//get input on submit
-//validate input
-
-//on page load?
-// function showContent() {
-
-// }
-
-var form = document.getElementById("project-input")! as HTMLTemplateElement;
-var formNode = form.content.cloneNode(true);
-document.getElementById("app")?.appendChild(formNode);
-
-var projectList = document.getElementById(
-  "project-list"
-)! as HTMLTemplateElement;
-var projectListNode = projectList.content.cloneNode(true);
-document.getElementById("app")?.appendChild(projectListNode);
-
-// function onSubmit() {
-
-//     let title: string;
-//     let description: string;
-//     let people: number;
-
-//     const project = new ProjectInput(title, description, people);
-// }
-
+//make singleton
 class ProjectInput {
-  private _title: string;
-  private _description: string;
-  private _people: number;
+  formElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
 
-  constructor(t: string, d: string, p: number) {
-    this._title = t;
-    this._description = d;
-    this._people = p;
-  }
+  //   private _title: string;
+  //   private _description: string;
+  //   private _people: number;
 
-  AddProjectToList() {
-    console.log("lets start printing stuff");
-    let projectDiv = document.createElement("div");
-    let titleP = document.createElement("p");
-    let descriptionP = document.createElement("p");
-    let peopleP = document.createElement("p");
+  //   constructor(t: string, d: string, p: number) {
+  //     this._title = t;
+  //     this._description = d;
+  //     this._people = p;
+  //   }
 
-    let titleNode = document.createTextNode("title: " + this._title);
-    let descriptionNode = document.createTextNode(
-      "description: " + this._description
-    );
-    let peopleNode = document.createTextNode("people: " + this._people);
-
-    titleP.appendChild(titleNode);
-    descriptionP.appendChild(descriptionNode);
-    peopleP.appendChild(peopleNode);
-
-    projectDiv.appendChild(titleP);
-    projectDiv.appendChild(descriptionP);
-    projectDiv.appendChild(peopleP);
-    console.log("lets finish printing project");
-
-    document.getElementById("single-project")?.appendChild(projectDiv);
-  }
-
-  AddProject() {
-    var project = document.getElementById(
-      "single-project"
+  constructor() {
+    this.formElement = document.getElementById(
+      "project-input"
     )! as HTMLTemplateElement;
-
-    let titleP = document.createElement("p");
-    let descriptionP = document.createElement("p");
-    let peopleP = document.createElement("p");
-
-    let titleNode = document.createTextNode("title: " + this._title);
-    let descriptionNode = document.createTextNode(
-      "description: " + this._description
-    );
-    let peopleNode = document.createTextNode("people: " + this._people);
-
-    titleP.appendChild(titleNode);
-    descriptionP.appendChild(descriptionNode);
-    peopleP.appendChild(peopleNode);
-
-    project.appendChild(titleP);
-    project.appendChild(descriptionP);
-    project.appendChild(peopleP);
-
-    var projectNode = project.content.cloneNode(true);
-
-    projectNode.appendChild(project);
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+    const formContent = this.formElement.content.cloneNode(true);
+    this.hostElement.appendChild(formContent);
   }
 }
 
-const p1 = new ProjectInput(
-  "catology",
-  "learn all about cats, they're fun",
-  235682
-);
-const p2 = new ProjectInput(
-  "The study of medi-evil water collecting.",
-  "It's well boring",
-  0
-);
-
-p1.AddProjectToList();
-p2.AddProjectToList();
-
-p2.AddProject();
+const projectInput = new ProjectInput();
