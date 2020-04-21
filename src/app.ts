cfecb2e579ad0172a660b1cb265d68f7f7e95484
Zak Miller <Zak.Miller@bjss.com>
2020-04-21 09:59:00 +0100

    drag events

--- src/app.ts
+++ src/app.ts
@@ -176,11 +176,20 @@ class PostitListComponent extends Component<HTMLDivElement, HTMLElement>
 
   @autobind
   dragOverHandler(event: DragEvent) {
-    const listEl = this.element.querySelector("ul")!;
-    listEl.classList.add("droppable");
+    if (event.dataTransfer && event.dataTransfer.types[0] === "text/plain") {
+      event.preventDefault();
+      const listEl = this.element.querySelector("ul")!;
+      listEl.classList.add("droppable");
+    }
   }
 
-  dropHandler(_: DragEvent) {}
+  dropHandler(event: DragEvent) {
+    console.log(event.dataTransfer!.getData("text/plain"));
+    console.log(event);
+    console.log(event.target);
+
+    // postits.
+  }
 
   @autobind
   dragLeaveHandler(event: DragEvent) {
@@ -233,7 +242,8 @@ class PostitComponent extends Component<HTMLUListElement, HTMLLIElement>
 
   @autobind
   dragStartHandler(event: DragEvent) {
-    console.log(event);
+    event.dataTransfer!.setData("text/plain", this.postit.id);
+    event.dataTransfer!.effectAllowed = "move";
   }
 
   dragEndHandler(_: DragEvent) {
